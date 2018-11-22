30b91b70806ac74a3b9554af2b42e4e4e9c2e7a7
haochenchn <haochenchn@163.com>
2018-11-22 15:44:28 +0800

    index

--- src/main/java/com/ch/dataclean/web/ViewController.java
+++ src/main/java/com/ch/dataclean/web/ViewController.java
@@ -8,10 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Aaron on 2018/11/20
  */
 @Controller
-@RequestMapping("/view")
 public class ViewController {
-    @RequestMapping(value = "/index1")
-    public String toIndex1(){
-        return "uploadStatus";
+    @RequestMapping(value = "/")
+    public String index(){
+        return "forward:file/toUpload";
     }
 }
